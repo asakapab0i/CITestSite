2e979459240c0fc0883b03d9e9385d657de9494b
Bryan <bryan@inventory-sys.com>
2012-06-20 22:48:00 +0800

    Added login page for practice.

--- application/config/config.php
+++ application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = '3135455321545646878979321316489';
 
 /*
 |--------------------------------------------------------------------------
